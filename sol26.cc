9b328c5c6c9334d90c9b2ff4e4fdeeb25c4e590a
jtymburski <joshtym@users.noreply.github.com>
2024-07-26 15:26:31 -0600

    Finished version of solution to euler problem 26

--- sol26.cc
+++ sol26.cc
@@ -3,23 +3,44 @@
 #include <algorithm>
 
 int getLongestRepeatingDenominator(int);
-std::string getStringDecimal(int, int, bool&);
+std::string getStringDecimal(int, int, bool&, int&);
 std::string determineRepeatingDecimalStatus(std::string, bool&, int&);
 
 int main(int argc, char** argv)
 {
    bool isRepeating = false;
-   std::cout << getStringDecimal(1, 6, isRepeating) << std::endl;
-   std::cout << isRepeating << std::endl;
+   int startingIndex = 0;
+   std::cout << getLongestRepeatingDenominator(1000) << std::endl;
    return 0;
 }
 
 int getLongestRepeatingDenominator(int upperLimit)
 {
-   return -1;
+   int maxPeriod = 0;
+   int longestRepeatingDenominator = 0;
+   for (int i = 1; i < upperLimit; ++i)
+   {
+      if (i % 2 == 0)
+         continue;
+      if (i % 5 == 0)
+         continue;
+      
+      bool isRepeating = false;
+      int startIndexOfPeriod = -1;
+      std::string decimalOfFraction = getStringDecimal(1, i, isRepeating, startIndexOfPeriod);
+
+      if (isRepeating && (decimalOfFraction.length() - startIndexOfPeriod) > maxPeriod)
+      {
+         maxPeriod = decimalOfFraction.length() - startIndexOfPeriod;
+         longestRepeatingDenominator = i;
+      }
+   }
+   return longestRepeatingDenominator;
 }
 
-std::string getStringDecimal(int numerator, int denominator, bool& isRepeating)
+// This function is a programmed version of long manual long division. In order to determine if a decimal is repeating,
+// we check every 2000 digits to see if we've determined a repeating decimal yet
+std::string getStringDecimal(int numerator, int denominator, bool& isRepeating, int& startIndexOfRepeat)
 {
    int indexOfNumeratorDigit = 0;
    std::string numeratorAsString = std::to_string(numerator);
@@ -34,7 +55,8 @@ std::string getStringDecimal(int numerator, int denominator, bool& isRepeating)
    {
       if (iteration % 2000 == 0)
       {
-         number = determineRepeatingDecimalStatus(number, isRepeating);
+         int startIndex = 0;
+         number = determineRepeatingDecimalStatus(number, isRepeating, startIndexOfRepeat);
          if (isRepeating)
             break;
          iteration = 1;
@@ -69,6 +91,7 @@ std::string getStringDecimal(int numerator, int denominator, bool& isRepeating)
    return number;
 }
 
+// This function takes the current number in and returns a stripped version of the number (if it's a repeating decimal)
 std::string determineRepeatingDecimalStatus(std::string number, bool& isRepeating, int& startOfRepeatIndex)
 {
    int startingIndex = number.find('.') + 1;
@@ -76,7 +99,7 @@ std::string determineRepeatingDecimalStatus(std::string number, bool& isRepeatin
 
    for (int i = startingIndex; i < maxStartIndex; ++i)
    {
-      int maxLength = (i - startingIndex) / 2;
+      int maxLength = (number.length() - i) / 2;
 
       for (int j = 1; j <= maxLength; ++j)
       {
@@ -85,9 +108,23 @@ std::string determineRepeatingDecimalStatus(std::string number, bool& isRepeatin
          for (int k = i; k < j + i; ++k)
             repeatingNumToTry += number[k];
 
-         std::string comparingNumber = number.substr(0, i); 
+         std::string comparingNumber = number.substr(0, i);
+         
+         while (comparingNumber.length() < number.length())
+            comparingNumber += repeatingNumToTry;
+         
+         comparingNumber = comparingNumber.substr(0, number.length());
+
+         if (comparingNumber == number)
+         {
+            isRepeating = true;
+            startOfRepeatIndex = i;
+            return number.substr(0, i) + repeatingNumToTry;
+         }
       }
    }
 
+   isRepeating = false;
+   startOfRepeatIndex = -1;
    return number;
 }
