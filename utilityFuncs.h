7b52cc8cc55308b4a2a9b5f24cc39f21eaef9704
jtymburski <joshtym@users.noreply.github.com>
2024-07-26 15:26:29 -0600

    Further optimization to the prime finder

--- utilityFuncs.h
+++ utilityFuncs.h
@@ -3,22 +3,21 @@
 #include <cmath>
 #include <algorithm>
 
-// Use of the seive for finding primes (this can be optimized)
+// Use of the seive for finding primes
 std::vector<long long int> getPrimesUpToNumber(long long int number)
 {
    long double sqrtOfNumber = sqrt(number);
    std::vector<long long int> primes = {3};
-   std::vector<long long int> additionalPrimes;
+   std::vector<long long int>::iterator endOfInitalPrimes;
 
-   for (long long int i = 3; i <= sqrtOfNumber; ++i)
-      if (std::all_of(primes.begin(), primes.end(), [](long long int prime){return i % prime != 0;})
-         primes.push_back(i);
+   for (long long int i = 3; i <= number; ++i)
+   {
+      if (i <= sqrtOfNumber)
+         endOfInitialPrimes = primes.end();
          
-   for (long long int = floor(sqrtOfNumber) + 1; i <= number; ++i)
-      if (std::all_of(primes.begin(), primes.end(), [](long long int prime){return i % prime != 0;})
-         additionalPrimes.push_back(i);
-            
-    primes.insert(primes.end(), additionalPrimes.begin(), additionalPrimes.end());
+      if (std::all_of(primes.begin(), endOfInitialPrimes, [](long long int prime){return i % prime != 0;})
+         primes.push_back(i);
+   }
 
    return primes;
 }
